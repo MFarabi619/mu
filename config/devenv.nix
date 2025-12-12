8b0ecd709f83f8d15045793c0f229c9551f56343
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-11 19:52:32 -0500

    build(config/languages/c): enable module
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ config/devenv.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  imports = [
+    ./languages
+    ./packages.nix
+  ];
+
+  env = {
+    GREET = "devenv";
+  };
+
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # services.postgres.enable = true;
+
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello         # Run scripts directly
+    git --version # Use packages
+  '';
+
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+}
